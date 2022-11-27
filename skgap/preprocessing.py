4c7396c54ba843064839bcb4cf24831c6cde36e0
meow <ivy8792@gmail.com>
2022-11-28 00:06:34 +0800

    feat: Add basic tokenization

--- skgap/preprocessing.py
+++ skgap/preprocessing.py
@@ -13,9 +13,11 @@
 # * The global Term frequence
 
 import PyPDF2
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 # read review paper and extract the text
 filename = 'Review of information extraction technologies and applications'
+# filename = 'test'
 pdf = open(f'{filename}.pdf', 'rb')
 reader = PyPDF2.PdfFileReader(pdf, False)  # create a pdf reader object
 
@@ -26,9 +28,9 @@ reader = PyPDF2.PdfFileReader(pdf, False)  # create a pdf reader object
 #     title = reader.getOutlines()[0]['/Title']
 # except:
 #     title = reader.getDocumentInfo()['/Title']
-
-# extract text
 title = filename
+
+# extract text (GROBID, another method to parse research paper using pretrained model, fail to connect the server)
 num_pages = reader.numPages  # number of pages
 
 content_str = ""  # store the full text after adding '\n' for title detection
@@ -86,13 +88,68 @@ for page in range(num_pages):
             break
         else:
             content_str += t
-
-# 嘗試 pdf extractor github code
-
-# split out each paragraph
-paragraph = {}  # {title: content}
-
-
-
+    content_str += " "  # separate different pages
 
 pdf.close()
+
+# basic preprocessing
+def tokenization(ori_text):
+    new_text = ""
+    is_parentheses = False
+    ori_text = ori_text.lower()  # lowercasting
+    
+    for i, t in enumerate(ori_text):
+        if is_parentheses:
+            is_parentheses = False
+        elif t == '(':  # skip the words in the ()
+            is_parentheses = True
+        elif not t.isalpha():  # remove numbers and marks (didn't consider dash)
+            new_text += " "
+        else:
+            new_text += t
+    
+    return list(filter(None, new_text.split(' ')))
+    
+def normalize_process(word, stopwords):
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    
+    lem = lemmatizer.lemmatize(word)
+    stm = stemmer.stem(lem)
+    if stm not in stopwords and lem not in stopwords and word not in stopwords:
+        return stm
+    else:
+        return None
+    
+def normalization(token_list, stopwords):
+    new_list = []
+    
+    for t in token_list:
+        if len(t) == 1:  # skip only one letter
+            continue
+        else:
+            w = normalize_process(t, stopwords)
+            if w != None:
+                new_list.append(w)
+
+    return new_list
+    
+# main
+stopwords = ['me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 
+             'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against',
+             'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most',
+             'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 'can', 'will', 'just', 'don', 'should', 'now', 'also', 'as', 'e-mail', 'et', 'al']
+
+for sec in content:
+    tokens = tokenization(sec)
+    terms = normalization(tokens, stopwords)
+    
+
+'''
+Note
+1. 沒有考慮 dash
+2. 頁首或頁尾會印有出版書刊, 日期, doi 等，還沒刪掉
+3. 每篇 paper 格式都不同，PyPDF2 爬到的東西只有純文字，chapter number 都是用 naive 的 rule 去寫，無法 cover 到所有 paper
+4. 呈上，有 pretrained model (見上) 可用，但跑不起來 (連不到 server)
+5. 呈上上，title 亦然，每篇 paper 存 title 的方法都不一樣
+'''
